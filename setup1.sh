605432e8f46cf1437fd66b8e407557f352bc40d9
nurik23 <nurdoolot.tagaev>
2018-01-15 20:49:52 +0600

    ready

--- setup1.sh
+++ setup1.sh
@@ -7,7 +7,6 @@ if [ "$?" != "0" ]; then
 else [ "$?" == "0" ]            
    echo "Ping successful"
 fi
-
 }
 
 Vagrant(){
@@ -17,12 +16,12 @@ if [ "$?" != "0" ]; then
    echo -n "Enter Yes if you wont to install vagrant :  "
    read x
    if [ $x == "Yes" ];then
-   sudo apt install vagrant -y
+   wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb	
+   sudo dpkg -i vagrant_2.0.1_x86_64.deb
    fi 
 else [ "$?" == "0" ]            
    echo "$var"| tail -n1
 fi
-
 }
 
 Libvirt(){
@@ -32,37 +31,40 @@ if [ "$?" != "0" ]; then
    echo -n "Enter Yes if you wont to install libvert-bin : "
    read x
    if [ $x == "Yes" ];then
-   sudo apt install -y libvirt-bin
+   sudo apt install qemu-kvm libvirt-bin
+   sudo apt-get -f install
+   sudo adduser $USER libvirtd
    fi 
 else [ "$?" == "0" ]            
-   echo "virsh version: $lib"| tail -n1
+   echo "Libvirt version: $lib"| tail -n1
 fi
 }
 
 VagrantLibvirt(){
-VL=`vagrant-libvirt`
+VL=`sudo vagrant plugin list`
 if [ "$?" != "0" ]; then
    echo " 'vagrant-libvirt' is currently not installed "
    echo -n "Enter Yes if you wont to install vagrant-libvirt : "
    read x
    if [ $x == "Yes" ];then
+   wget http://production.cf.rubygems.org/rubygems/rubygems-update-2.0.3.gem
+   gem install rubygems-update-2.0.3.gem
+   sudo update_rubygems
    sudo apt-get build-dep vagrant ruby-libvirt
    sudo apt-get install -y qemu libvirt-bin ebtables dnsmasq
    sudo apt-get install -y libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev
    sudo vagrant plugin install vagrant-libvirt 
    fi 
-else [ "$?" == "0" ]            
-   echo "$VL"| tail -n1
+else [ "$?" == "0" ]
+   echo "vagrant plugin list :"            
+   echo "$VL"
 fi
 }
 
 
 Bridge(){
 echo "bridge device on local machine : "
-brctl show | awk 'FNR == 2 {print $1}'
-
-echo "a bridge is :"
-ifconfig | awk 'FNR == 2 {print $1}' 
+brctl show ##| awk 'FNR == 2 {print $1}'
 }
 
 
